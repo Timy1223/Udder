1de445dc72058d3dda55f20f122de538152c1889
Timy1223 <timothyeh666@gmail.com>
2023-07-20 00:18:05 +0800

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Timmy Yeh on 2023/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Timmy Yeh on 2023/7/20.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
